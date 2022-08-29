d61ee9c70eb17423f039b7a93f823e43fe61538e
504 <504@DESKTOP-MG5OUD4>
2022-08-29 17:47:18 +0900

    8/29 17:46 문의사항 js기능 부여 (CSS 더해야함)

--- webproject_sobremesa/public/Customer/customer.js
+++ webproject_sobremesa/public/Customer/customer.js
@@ -12,11 +12,12 @@ let customers=[]
 
 출력()
 
+
 function 데이터저장(){
 	const customer={
 		type: document.querySelector('.form_type').value,
 		title: document.querySelector('.form_title').value,
-		
+		content: document.querySelector('.form_content').value
 		
 	}
 	alert('저장완료')
@@ -24,7 +25,7 @@ function 데이터저장(){
 	customers.push(customer)
 	console.log(customer.type)
 	
-	document.querySelector('.form_type').value=''
+	
 	document.querySelector('.form_title').value=''
 	document.querySelector('.form_content').value=''
 	document.querySelector('.form_email').value=''
@@ -32,23 +33,37 @@ function 데이터저장(){
 	
 	
 	출력()
+	
 }
 
 
 function 출력(){
-	let html=`<tr><th>순번</th><th>문의유형</th><th>제목</th></tr>`
+	let html=`<tr><th><a href='#''>순번</a></th><th><a href='#'>문의유형</a></th><th><a href='#'>제목</th></a></tr>`
 	
 	for(let i=0; i<customers.length; i++){
 		html+=`	<tr>
-					<th>${[i+1]}</th>
-					<th>${customers[i].type}</th>
-					<th>${customers[i].title}</th>
+					<th><a href='#' onclick='펼치기(${i})'>${[i+1]}</a></th>
+					<th><a href='#' onclick='펼치기(${i})'>${customers[i].type}</a></th>
+					<th><a href='#' onclick='펼치기(${i})'>${customers[i].title}</a></th>
 				</tr>`
 	}
 
 	
 	document.querySelector('.result').innerHTML= html
-	
+}
+
+
+
+function 펼치기(i){
+	document.querySelector('.js_title').innerHTML= customers[i].title
+	document.querySelector('.js_content').innerHTML= customers[i].content
 	
 }
+
+
+
+
+
+
+
 
